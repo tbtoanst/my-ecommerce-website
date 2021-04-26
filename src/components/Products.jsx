278c3df953793389fecdbd5eccbf36b4854f2bff
tbtoanst <tbtoanst@gmail.com>
2021-04-27 00:47:00 +0700

    create project step

---
+++ src/components/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Grid} from '@material-ui/core'
+import Product from './Product/Product'
+
+const Products = () => {
+    return (
+        <div>
+            <h1 className="title">Products</h1>
+            <Product />
+        </div>
+    )
+}
+
+export default Products;
